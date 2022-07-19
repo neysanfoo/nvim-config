695b8c55306783ef2b6563e268820b15ca787fa7
Neysan Foo <neysanfoo@gmail.com>
2022-07-20 01:49:57 +0800

    added projects

--- lua/neysan/keymaps.lua
+++ lua/neysan/keymaps.lua
@@ -78,6 +78,7 @@ keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
 keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
 keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
 
 -- Comment
 keymap("n", "<C-/>", "<cmd>lua require('Comment.api').toggle_current_linewise()<CR>", opts)
